0534d05e3ae47bffe01b5ef010f41cc602259a9e
Dilip Choudhary <dileepchoudhary469@gmail.com>
2024-03-25 12:55:01 +0530

    change address from localhost to strapi deployed server address

--- utils/GlobalAPI.tsx
+++ utils/GlobalAPI.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 const api_key=process.env.NEXT_PUBLIC_API_KEY;
 const axiosClient=axios.create(
   {
-    baseURL:"http://localhost:1337/api/",
+    baseURL:"https://doctor-appointment-system-backend-zk50.onrender.com/api/",
     headers:{
       Authorization:`Bearer ${api_key}`
     }
