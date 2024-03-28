a6e55740ec47f31dd1d5cf6e4158995a96e4ac7f
Dilip Choudhary <dileepchoudhary469@gmail.com>
2024-03-28 16:08:19 +0530

    added key

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -68,7 +68,7 @@ function SearchBar() {
                   <div className='md:w-[400px] w-[250px] h-[250px] md:h-[400px] px-3 flex flex-col gap-2 items-center '>
                     {doctorsList?.filter((doctor)=>search.toLowerCase()==='' ? doctor : doctor.attributes.name.toLowerCase().includes(search)).map((doctor)=>{
                     return (
-                      <Link className='border-secondary shadow-md transition-all border-[1px] hover:border-none  cursor-pointer active:scale-95 hover:bg-secondary flex  items-center justify-start gap-2 w-full p-2 rounded-md md:rounded-xl' href={`/details/${doctor.id}`}>
+                      <Link key={doctor.id} className='border-secondary shadow-md transition-all border-[1px] hover:border-none  cursor-pointer active:scale-95 hover:bg-secondary flex  items-center justify-start gap-2 w-full p-2 rounded-md md:rounded-xl' href={`/details/${doctor.id}`}>
                         <Image className=' h-10 w-10 rounded-full' src={doctor.attributes.image.data.attributes.url} height={60} width={60} alt={doctor.attributes.name}/>
                         <label className=' cursor-pointer'> {doctor.attributes.name}</label>
                       
